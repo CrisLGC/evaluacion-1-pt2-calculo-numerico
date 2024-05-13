4735b8088c2a08c107038f0b75b93041d83da739
Cristian <cristianguevara0331@gmail.com>
2024-05-13 18:14:44 -0400

    Agregue la funcionalidad de la eliminacion gausseana ademas de que empece a desarrollar un texto que te avise si lo que ingresaste es valido

--- main.py
+++ main.py
@@ -15,30 +15,84 @@ def gauss_jordan(A, b):
                     Ab[j] = Ab[j] - Ab[i] * Ab[j, i]
         x = Ab[:, -1]
         return x
+
+def eliminacion_gausseana(A, b):
+    n = len(b)
+
+    for i in range(n):
+        A[i] = A[i] / A[i, i]
+
+        for j in range(i+1, n):
+            A[j] = A[j] - A[i] * A[j, i]
+
+    x = np.zeros(n)
+    for i in range(n-1, -1, -1):
+        x[i] = b[i] - sum(A[i, j]*x[j] for j in range(i+1, n))
+
+    return x
+
+
+
+
     
      
 
 def main(page: ft.Page):
     def resolver_clicked(e):
-       
-         A = np.array(eval(A_input.value))
-         b = np.array(eval(b_input.value))
+        validate_input(str(A_input.value))
+        validate_input(str(b_input.value))
+
+        if dd.value=="Gauss-Jordan":
+            A = np.array(eval(A_input.value))
+            b = np.array(eval(b_input.value))
+            x = gauss_jordan(A, b)
+            t = ft.Text(value= str(x), color="green")
+            page.controls.append(t)
+            page.update()
+        elif dd.value=="Eliminacion Gausseana":
+            A = np.array(eval(A_input.value))
+            b = np.array(eval(b_input.value))
+            x = eliminacion_gausseana(A, b)
+            t = ft.Text(value= str(x), color="green")
+            page.controls.append(t)
+            page.update()
+
         
-         x = gauss_jordan(A, b)
-        
-         t = ft.Text(value= str(x), color="green")
-         page.controls.append(t)
-         page.update()
     page.title = "Gauss-Jordan"
+    def elegirop(e):
+        A_input.value=""
+        b_input.value=""
+        A_input.focus()
+        A_input.update()
+        b_input.update()
+
+    def validate_input(input_text):
+        allowed_chars = set("[] ,.0 1 2 3 4 5 6 7 8 9 -")
+        if set(input_text) - allowed_chars:
+            indicacion.value="invalido"
+            indicacion.update()
+        else:
+            indicacion.value="valido"
+            indicacion.update()
+
+
 
     A_input = ft.TextField(hint_text="Introduce la matriz A como una lista de listas")
     b_input = ft.TextField(hint_text="Introduce el vector b como una lista")
+    indicacion = ft.Text(value="INGRESAR MATRICES Y VECTORES COMO UNA LISTA DE LISTAS. EJEMPLO: matriz: [[0, 0], [0, 0]] vector: [0, 0]", color="yellow")
     
 
     resolver_button = ft.ElevatedButton("Resolver", on_click=resolver_clicked)
 
    
-    page.add(A_input, b_input, resolver_button)
+    page.add(A_input, b_input, indicacion, resolver_button)
+    dd = ft.Dropdown(width=300,options=[
+            ft.dropdown.Option("Gauss-Jordan", on_click=elegirop),
+            ft.dropdown.Option("Eliminacion Gausseana", on_click=elegirop),
+
+        ],
+    )
+    page.add(dd)
 
   
 
